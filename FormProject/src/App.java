29d6815b2b54a8d7e760d6670382ecc74e020935
Filipe Pedroso <filipebruhnspedroso@gmail.com>
2022-08-09 16:39:18 -0300

    continuing the project

--- FormProject/src/App.java
+++ FormProject/src/App.java
@@ -1,7 +1,5 @@
 import java.util.Scanner;
 
-import java.util.Scanner;
-
 public class App {
     public static void main(String[] args) {
         
@@ -9,6 +7,17 @@ public class App {
         NewUser user = new NewUser();
 
         System.out.print("Type your first name: ");
-        
+        user.setFirstName(input.nextLine());
+
+        System.out.print("Type your last name:");
+        user.setLastName(input.nextLine());
+
+        System.out.print("Type your e-mail:");
+        user.setEmail(input.nextLine());
+
+        System.out.print("Type your password:");
+        user.setPassword(input.nextLine());
+
+        System.out.println("Welcome "+user.getFirstName()+" "+user.getLastName()+",\nThank you for registering on our website!"+"\nTo validate your account follow the step by step sent to "+user.getEmail());
     }
 }
--- FormProject/src/NewUser.java
+++ FormProject/src/NewUser.java
@@ -1,3 +1,36 @@
 public class NewUser {
     private String firstName, lastName, email, password;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    
 }
